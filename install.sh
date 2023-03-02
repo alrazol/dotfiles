a7097e8c39831f5650a24b54a1b968b7be67000f
Bruno Lajoie <brunolajoie@gmail.com>
2023-03-02 12:45:33 +0100

    add more zsh plugins and theme

--- install.sh
+++ install.sh
@@ -21,9 +21,9 @@ symlink() {
   fi
 }
 
-# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
-# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
-for name in aliases gitconfig irbrc rspec zprofile zshrc direnvrc p10k.zsh vimrc; do
+# For some files `$name` in the present folder (except `*.sh`, `README.md` and `config`),
+# backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+for name in aliases gitconfig zprofile zshrc direnvrc p10k.zsh; do
   if [ ! -d "$name" ]; then
     target="$HOME/.$name"
     backup $target
@@ -31,17 +31,44 @@ for name in aliases gitconfig irbrc rspec zprofile zshrc direnvrc p10k.zsh vimrc
   fi
 done
 
-# Install zsh-syntax-highlighting plugin
+# Install some ZSH plugins
 CURRENT_DIR=`pwd`
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting
 fi
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
+  echo "-----> Installing zsh plugin 'zsh-autosuggestions'..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+fi
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-completions" ]; then
+  echo "-----> Installing zsh plugin 'zsh-completions'..."
+  git clone https://github.com/zsh-users/zsh-completions
+fi
+
+# Install some ZSH theme
+ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
+mkdir -p "$ZSH_THEMES_DIR" && cd "$ZSH_THEMES_DIR"
+if [ ! -d "$ZSH_THEMES_DIR/powerlevel10k" ]; then
+  echo "-----> Installing zsh theme powerlevel10k..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+fi
+
 cd "$CURRENT_DIR"
 
+# Sublime Text - Mac OS only
+if [[ `uname` =~ "Darwin" ]]; then
+  SUBLIME_PATH=~/Library/Application\ Support/Sublime\ Text
+  mkdir -p "$SUBLIME_PATH/Packages/User" "$SUBLIME_PATH/Installed Packages"
+  backup "$SUBLIME_PATH/Pacakges/User/Preferences.sublime-settings"
+  backup "$SUBLIME_PATH/Pacakges/User/Package Control.sublime-settings"
+  ln -s $PWD/Preferences.sublime-settings $SUBLIME_PATH/Pacakges/User/Preferences.sublime-settings
+  ln -s $PWD/Preferences.Package Control.sublime-settings $SUBLIME_PATH/Pacakges/User/Preferences.sublime-settings
+fi
+
+# VScode
 # USE VSCODE "SETTING SYNC" FEATURE INSTEAD OF DOTFILES TO SYNC `settings.json` and `keybindings.json`
 # # Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
 # # If it's a macOS
@@ -67,7 +94,7 @@ if [[ `uname` =~ "Darwin" ]]; then
   target=~/.ssh/config
   backup $target
   symlink $PWD/config $target
-  ssh-add -K ~/.ssh/id_ed25519
+  ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 fi
 
 # Refresh the current terminal with the newly installed configuration
